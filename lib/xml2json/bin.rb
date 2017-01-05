d397143819dd278b7b018fa112f24be928d39cc4
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2017-01-05 22:11:35 +0900

    bundle gem xml2json-bin

---
+++ lib/xml2json/bin.rb
@@ -0,0 +1,7 @@
+require "xml2json/bin/version"
+
+module Xml2json
+  module Bin
+    # Your code goes here...
+  end
+end
---
+++ lib/xml2json/bin/version.rb
@@ -0,0 +1,5 @@
+module Xml2json
+  module Bin
+    VERSION = "0.1.0"
+  end
+end
