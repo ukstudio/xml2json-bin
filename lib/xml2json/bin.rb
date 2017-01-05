be487ac7a3063bee1d48af447371aacf41ce7067
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2017-01-05 23:04:39 +0900

    Implment xml2json-bin
    usage
    
    ```
    cat foo.xml | xml2json-bin
    ```

--- lib/xml2json/bin.rb
+++ lib/xml2json/bin.rb
@@ -1,7 +1,17 @@
 require "xml2json/bin/version"
+require "active_support/json"
+require "active_support/core_ext/hash/conversions"
 
 module Xml2json
   module Bin
-    # Your code goes here...
+    class Converter
+      def initialize(xml)
+        @hash = Hash.from_xml(xml)
+      end
+
+      def to_json
+        @hash.to_json
+      end
+    end
   end
 end
